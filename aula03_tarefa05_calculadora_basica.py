f66995dea6c936c1dcc7a50f8415c26bbb84d322
luizz <luizepperdigao@gmail.com>
2025-03-07 20:51:29 -0300

    Add files via upload

---
+++ aula03_tarefa05_calculadora_basica.py
@@ -0,0 +1,11 @@
+a=float(input("Digite o primeiro valor: "))
+b=float(input("Digite o segundo valor: "))
+soma=a+b
+sub=a-b
+multiplicação=a*b
+divisão_real=a/b
+
+print(f"oresultado da soma é {soma}")
+print(f"o resultado da subtração é {sub}")
+print(f"o resultado da multiplicação é {multiplicação}")
+print(f"o resultado da divisão real é {divisão_real}")
