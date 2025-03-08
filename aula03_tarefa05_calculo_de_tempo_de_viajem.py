92b838e668dfd14dd6e7b8bd238df5bcb11a372a
luizz <luizepperdigao@gmail.com>
2025-03-07 21:09:29 -0300

    Add files via upload

---
+++ aula03_tarefa05_calculo_de_tempo_de_viajem.py
@@ -0,0 +1,4 @@
+distancia=float(input("Digite a distancia em km entre o ponto de partida e o ponto de chegada: "))
+velocidade_media=float(input("Digite a velocidade media em km/h percorrida no trajeto"))
+divisão_real=distancia/velocidade_media
+print(f"o tempo medio de viajem é igual a: {divisão_real} horas")
