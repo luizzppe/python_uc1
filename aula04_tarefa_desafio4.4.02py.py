0290508d880b0d2bed49350575fdd4bd25d0995b
luizz <luizepperdigao@gmail.com>
2025-03-12 19:03:41 -0300

    Add files via upload

---
+++ aula04_tarefa_desafio4.4.02py.py
@@ -0,0 +1,7 @@
+numero=int(input("Digite um número inteiro para saber se ele é positivo, negativo ou zero "))
+if numero >= 1:
+    print("Seu número é positivo!")
+elif numero <= -1:
+    print("Seu número é negativo!")
+else:
+    print("Seu número é 0!")
