b08363127c465b384279b49d533258c538fc04c5
luizz <luizepperdigao@gmail.com>
2025-03-12 19:14:40 -0300

    Add files via upload

---
+++ aula04_tarefa_desafio4.4.03py.py
@@ -0,0 +1,6 @@
+primeiro=int(input("Insira dois numeros e exibirei o maior deles (não podem ser iguais!!). Digite o primeiro número:"))
+segundo=int(input("Digite o segundo numero: "))
+if segundo >= primeiro:
+    print(f"O maior número é: {segundo}")
+else:
+    print(f"O maior número é: {primeiro}") 
