701e83fe17673e38d1760b11234e1ae19d09d4d8
luizz <luizepperdigao@gmail.com>
2025-03-12 21:18:11 -0300

    Add files via upload

---
+++ aula05_tarefa03.py
@@ -0,0 +1,6 @@
+idade = int(input("Digite sua idade: "))
+senha=str(input("Digite sua senha: ")) 
+if idade >= 18 and senha == ("bola"):
+    print("Acesso aprovado!")
+else:
+    print("Acesso negado!")
