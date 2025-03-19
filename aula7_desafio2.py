59fe16cc1cb68bc3c885703d452ac3cd5cb79cf4
luizz <luizepperdigao@gmail.com>
2025-03-19 20:51:28 -0300

    Add files via upload

---
+++ aula7_desafio2.py
@@ -0,0 +1,5 @@
+numero1=int(input("Digite um número para saber se ele é par ou impar: "))
+if numero1 %2 == 0:
+    print("Esse número é par!")
+elif numero1 %2 == 1:
+     print("Esse número é impar!")
