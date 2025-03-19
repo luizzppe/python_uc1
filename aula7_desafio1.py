e8114ef5adbf5e2a8752427b21bde775c70c985c
luizz <luizepperdigao@gmail.com>
2025-03-19 20:43:14 -0300

    Update aula7_desafio1.py

--- aula7_desafio1.py
+++ aula7_desafio1.py
@@ -1,4 +1,16 @@
+numero1= leia numero inserido peo usuario
+numero2= leia o numero inserido pelo usuario
+soma= some o numero1 e o numero2
+mostre a soma
+
+
 numero1=int(input("Digite um numero: "))
 numero2=int(input("Digite um numero: "))
 soma= numero1 + numero2
 print(f"o resultado da soma é: {soma}")
+
+
+P     N1     N2     S      R
+1     5      5      ?      ?
+2     5      5      10     ?
+3     5      5      10     10
