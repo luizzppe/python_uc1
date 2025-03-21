c18f7b123b8109cf67392bdc0bd6322d08a904e0
luizz <luizepperdigao@gmail.com>
2025-03-21 15:17:57 -0300

    Update aula7.desafio6.py

--- aula7.desafio6.py
+++ aula7.desafio6.py
@@ -1,2 +1,12 @@
+"""seq recebe os primeiros dez termos da sequencia de fibonacci, tornando-se uma tupla
+imprima a funcão (valor) de seq
+"""
+
 seq=(1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
 print(f"Os primeiros dez termos da Sequência de Fibonacci são: {seq}")
+
+"""
+P  Seq                                  Proc
+1   1, 1, 2, 3, 5, 8, 13, 21, 34, 55    leitura
+2   1, 1, 2, 3, 5, 8, 13, 21, 34, 55    imprimindo seq
+"""
