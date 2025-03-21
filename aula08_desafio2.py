f844e9b30e062585f4359fe39dc4e51462bd6393
luizz <luizepperdigao@gmail.com>
2025-03-21 20:05:46 -0300

    Add files via upload

---
+++ aula08_desafio2.py
@@ -0,0 +1,12 @@
+def soma(a,b):
+    return a + b
+def test_soma ():
+    resultado = soma(5, 7)
+    print("A soma é:", resultado)
+
+"""
+funçao soma (numeros)
+- recebe um vetor com numeros e calcula da soma dos mesmos
+"""
+if __name__ == "__main__":
+    test_soma ()
