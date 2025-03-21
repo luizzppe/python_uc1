355830fce127b0b0b46a254d905648c60fd32f7f
luizz <luizepperdigao@gmail.com>
2025-03-21 13:54:05 -0300

    Update aula7.desafio4.py

--- aula7.desafio4.py
+++ aula7.desafio4.py
@@ -1,3 +1,12 @@
+""" a variavel "numero" recebe o numero que o usuario deseja calcular o fatorial
+definindo a variavel "fatorial" como 1 (o expoente neutro damultiplicação) para realizar as multiplicações de "numero"
+enquanto numero for maior ou igual a um faça:
+"fatorial" é igual a "numero" vezes "fatorial"
+"numero" é igual a "numero" menos 1
+se nao
+mostre a função (nesse caso o valor) de fatorial final
+"""
+
 numero=int(input("Digite um numero que calcularei o fatorial: "))
 fatorial=1
 while numero>=1:
@@ -5,3 +14,5 @@ while numero>=1:
     numero=numero-1
 else:
     print(f"{fatorial}")
+
+
