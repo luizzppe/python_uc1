1191a803f464ae06a0e82e683ff94e604371e527
luizz <luizepperdigao@gmail.com>
2025-03-21 18:17:56 -0300

    Update aula7_desafio7.py

--- aula7_desafio7.py
+++ aula7_desafio7.py
@@ -1,3 +1,10 @@
+""" "numero" recebe a informaçao do usuario
+se o modulo de "numero" por 2 for igual a 0
+mostre esse numero é par 
+mas se o modulo de "numero" por 2 for 1 
+mostre esse numero é impar
+"""
+
 numero=int(input("Digite um numero e direi se ele é par ou impar: "))
 if numero%2==0:
     print("Esse numero é par!")
