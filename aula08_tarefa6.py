dc5ba4ea34821cd2f0ea1b43dc565617929ad2c6
luizz <luizepperdigao@gmail.com>
2025-03-21 20:45:51 -0300

    Add files via upload

---
+++ aula08_tarefa6.py
@@ -0,0 +1,17 @@
+def fatorial(n):
+    if n < 0:
+        return "Numero invalido para o calculos de fatorial"
+    elif n == 0:
+        return 1
+    else:
+        resultado=1
+        for i in range(1, n+1):
+            resultado*=i
+        return resultado
+def test_fatorial():
+    numero=int(input("\n\ntDigite um numero para que seja calculado o fatorial: "))
+    resultado=fatorial(numero)
+    print(f"\n\nt O fatorial de {numero} ({numero}!) é igual à {resultado}\n\n")
+
+if __name__ == "__main__":
+    test_fatorial()
