0cab8112ffb0042ec1f1056ac473209902d3b348
luizz <luizepperdigao@gmail.com>
2025-03-21 21:46:19 -0300

    Add files via upload

---
+++ aula08_desafio1.1.py
@@ -0,0 +1,17 @@
+import sys
+def calculador(n1, n2, op):
+    if op == "+":
+        resultado = n1+n2  
+    elif op == "-":
+        resultado= n1-n2  
+    elif op == "*":
+        resultado = n1*n2  
+    elif op == "/":
+        resultado= n1/n2   
+    print(f"{n1} {op} {n2} = {resultado}")
+if __name__ == "__main__":
+    argumentos=sys.argv[1:]
+    numero1=float(argumentos[0])
+    numero2=float(argumentos[1])
+    operacao=argumentos[2]
+    calculador (numero1, numero2, operacao )
